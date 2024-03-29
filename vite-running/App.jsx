3eca89176bd9cfdc8d0eb17cc73993f229138bb1
panfeng <2207375139@qq.com>
2024-03-29 09:22:39 +0800

    利用vite集成jsx

---
+++ vite-running/App.jsx
@@ -0,0 +1,6 @@
+import React from './core/React.js'
+const buttonEl = React.createButtonNode('button', null, '按钮')
+
+const App = <div id='id'>hi mini-react</div>
+
+export default App
---
+++ vite-running/main.jsx
@@ -0,0 +1,4 @@
+import ReactDom from './core/ReactDom.js'
+import App from './App.jsx'
+
+ReactDom.createElemet(document.querySelector('#root')).render(App)
